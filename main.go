23ce185e2c39d4da7a356c5fa5d9ea0e67c0632b
yusupovanton <antonyusupov@outlook.com>
2022-09-19 00:35:34 +0200

    Intermediate step in the server: first working image

--- main.go
+++ main.go
@@ -6,6 +6,40 @@ import (
 	"net/http"
 )
 
+
+func formhandler(w http.ResponseWriter, r *http.Request){
+	if err:= r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+
+}
+
 func main() {
-	
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formhandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err!=nil {
+		log.Fatal(err)
+	}
 }
